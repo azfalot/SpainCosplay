0c2e056ef01fb4dc5f187d8e8bb0b4d9127f9708
Carlos Otero Ramirez <cj_driking@hotmail.com>
2016-01-07 02:16:23 +0100

    Funcion OnClick de la lista eventos
    De momento solo lanza un toast

--- SpainCosplay3/app/src/main/java/coterodev/spaincosplay/ListaEventosActivity.java
+++ SpainCosplay3/app/src/main/java/coterodev/spaincosplay/ListaEventosActivity.java
@@ -4,7 +4,10 @@ import android.app.ListActivity;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
@@ -26,5 +29,15 @@ public class ListaEventosActivity extends ListActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_eventos);
         setListAdapter(new ArrayAdapter<String>(this,R.layout.objeto_lista,eventosNegocio.getNombres()));
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        // TODO Auto-generated method stub
+        //super.onListItemClick(l, v, position, id);
+        String selection = l.getItemAtPosition(position).toString();
+        //segun el texto seleccionado hacer una consulta para ese evento y cargar un layout con la informacion deseada
+        Toast.makeText(this, selection, Toast.LENGTH_LONG).show();
+
     }
 }
