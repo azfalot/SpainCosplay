7ba4c3dcb0a02866467d532a352c99359300309f
Carlos Otero Ramirez <cj_driking@hotmail.com>
2016-01-30 01:36:03 +0100

    Trasteando con las notificaciones
    Se viene la version baratie

--- SpainCosplay3/app/src/main/java/coterodev/spaincosplay/PrincipalActivity.java
+++ SpainCosplay3/app/src/main/java/coterodev/spaincosplay/PrincipalActivity.java
@@ -4,38 +4,37 @@ import android.app.Activity;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.content.res.Resources;
-import android.graphics.drawable.Icon;
-import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
-import com.facebook.FacebookSdk;
 
 import java.util.Locale;
 
 public class PrincipalActivity extends Activity {
     Toast toast;
     Locale myLocale;
+
     @Override
-    public void onCreate(Bundle IstanciaSalvada){
+    public void onCreate(Bundle IstanciaSalvada) {
         super.onCreate(IstanciaSalvada);
         //FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.principal_layout2);
         //Declaracion botones
-        Button verEventos = (Button)findViewById(R.id.verEventosBtn);
-        Button acercaDe = (Button)findViewById(R.id.acercaDeBtn);
-        Button salirBtn = (Button)findViewById(R.id.salirBtn);
-        Button ingles = (Button)findViewById(R.id.ingles);
-        Button español = (Button)findViewById(R.id.español);
-        Button japones = (Button)findViewById(R.id.japones);
-        Button compartir = (Button)findViewById(R.id.share);
+        Button verEventos = (Button) findViewById(R.id.verEventosBtn);
+        Button acercaDe = (Button) findViewById(R.id.acercaDeBtn);
+        Button salirBtn = (Button) findViewById(R.id.salirBtn);
+        Button ingles = (Button) findViewById(R.id.ingles);
+        Button español = (Button) findViewById(R.id.español);
+        Button japones = (Button) findViewById(R.id.japones);
+        Button compartir = (Button) findViewById(R.id.share);
 
        /* Button button3 = (Button)findViewById(R.id.cerrarSesionBtn);
         button3.setOnClickListener(new View.OnClickListener() {
@@ -88,10 +87,12 @@ public class PrincipalActivity extends Activity {
                 setLocale("ja");
             }
         });
+
+        CargarNota();
     }
 
-    public void lanzarVista(String parametro){
-        switch (parametro){
+    public void lanzarVista(String parametro) {
+        switch (parametro) {
             case "eventos":
                 Intent i = new Intent(PrincipalActivity.this, ListaEventosActivity.class);
                 startActivity(i);
@@ -122,38 +123,46 @@ public class PrincipalActivity extends Activity {
         }
     }
 
-    public void setLocale(String lang){
-            myLocale = new Locale(lang);
-            Resources res = getResources();
-            DisplayMetrics dm = res.getDisplayMetrics();
-            Configuration conf = res.getConfiguration();
-            conf.locale = myLocale;
-            res.updateConfiguration(conf, dm);
-            Intent refresh = new Intent(this, PrincipalActivity.class);
-            startActivity(refresh);
-            finish();
+    //Cambiar Idioma
+    public void setLocale(String lang) {
+        myLocale = new Locale(lang);
+        Resources res = getResources();
+        DisplayMetrics dm = res.getDisplayMetrics();
+        Configuration conf = res.getConfiguration();
+        conf.locale = myLocale;
+        res.updateConfiguration(conf, dm);
+        Intent refresh = new Intent(this, PrincipalActivity.class);
+        startActivity(refresh);
+        finish();
     }
 
-    public void cargarNotificacion(){
-        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        Notification notification;
-        final String appPackageName = getPackageName();
-        int icon = R.drawable.icono;
-        //Icon icono = Icon.createWithResource(getApplicationContext(),R.drawable.icono);
-        CharSequence tickerText = "Nueva Actualizacion Disponible";
-        long when = System.currentTimeMillis(); //cuando se lanza la nota
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN) {
-            notification = new Notification.Builder(getApplicationContext())
-                    .setContentText(tickerText)
-                    .setSmallIcon(icon)
-                   // .setLargeIcon(icono)
-                    .setWhen(when)
-                    .build();
+    public void CargarNota(){
+        NotificationCompat.Builder mBuilder =
+                (NotificationCompat.Builder) new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.ic_notify)
+                        .setContentTitle("My notification")
+                        .setContentText("Hello World!");
+// Creates an explicit intent for an Activity in your app
+       /* Intent resultIntent = new Intent(this, ResultActivity.class);
 
-        }
-        Intent notificationIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName));
-        PendingIntent contentIntent = PendingIntent.getActivity(this,0,notificationIntent,0);
-        notification.
-
+// The stack builder object will contain an artificial back stack for the
+// started Activity.
+// This ensures that navigating backward from the Activity leads out of
+// your application to the Home screen.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+// Adds the back stack for the Intent (but not the Intent itself)
+        stackBuilder.addParentStack(ResultActivity.class);
+// Adds the Intent that starts the Activity to the top of the stack
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        mBuilder.setContentIntent(resultPendingIntent);
+        */NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+// mId allows you to update the notification later on.
+        mNotificationManager.notify(getTaskId(), mBuilder.build());
     }
 }
